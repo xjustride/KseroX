f4b866c910a02017f2db8609b6d2c004f710450c
remigiusz.krupa <remigiusz.krupa@BR-09>
2023-04-27 14:21:18 +0200

    Dodaj pliki projektów.

---
+++ Zadanie1/Copier.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ver1
+{
+    public class Copier : BaseDevice, IPrinter, IScanner
+    {
+        public int PrintCounter { get; private set; } = 0;
+        public int ScanCounter { get; private set; } = 0;
+
+        public void Print(in IDocument document)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Scan(out IDocument document, IDocument.FormatType formatType = IDocument.FormatType.JPG)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ScanAndPrint()
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+    }
+}
---
+++ Zadanie1/Program.cs
@@ -0,0 +1,20 @@
+﻿using ver1;
+
+class Program
+{
+    static void Main()
+    {
+        var xerox = new Copier();
+        xerox.PowerOn();
+        IDocument doc1 = new PDFDocument("aaa.pdf");
+        xerox.Print(in doc1);
+
+        IDocument doc2;
+        xerox.Scan(out doc2);
+
+        xerox.ScanAndPrint();
+        System.Console.WriteLine(xerox.Counter);
+        System.Console.WriteLine(xerox.PrintCounter);
+        System.Console.WriteLine(xerox.ScanCounter);
+    }
+}
